65871d0f8bd7fbe6f31f2a5403ab10232d1002c9
YuiKyoko <kimberlyesquivelgala@gmail.com>
2016-09-14 13:42:40 -0500

    traductor

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesiòn";
+document.getElementById("inputEmail").placeholder="correo electronico";
+document.getElementById("inputPassword").placeholder="contraseña";
+document.querySelector("span").innerHTML="Recordar datos";
+document.querySelector("button").innerHTML="Iniciar sesion ";
